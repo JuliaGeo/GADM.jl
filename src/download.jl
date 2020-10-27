46e1f55128cea6801adf1a2aa93fcdaf3c20f5bf
Aayush Joglekar <aayushjog@gmail.com>
2020-10-27 22:33:09 +0530

    [download.jl] Implement regex and refactor code

--- src/download.jl
+++ src/download.jl
@@ -5,18 +5,11 @@ This function takes in a dataset code of format "GADM_<Country Code>".\n
 Returns the URL of the gpkg dataset of the country
 """
 function dataurl(dataid)
-    # Only accepts GADM Dataset Provider
-    if contains(dataid, "GADM")
-        try
-            provider, country_code = split(dataid, '_')
-            "https://biogeo.ucdavis.edu/data/gadm3.6/gpkg/gadm36_$(country_code)_gpkg.zip"
-        catch err
-            @error "invalid dataset format, please use GADM_<country_code>."
-            throw(ArgumentError("invalid dataset format"))
-        end
+    if occursin(r"GADM_\w+", dataid)
+        provider, country_code = split(dataid, '_')
+        "https://biogeo.ucdavis.edu/data/gadm3.6/gpkg/gadm36_$(country_code)_gpkg.zip"
     else
-        @error "dataset provider not supported, please try \"GADM_<country_code>\"."
-        throw(ArgumentError("dataset provider not supported"))
+        throw(ArgumentError("invalid dataid $dataid"))
     end
 end
 
@@ -25,21 +18,20 @@ end
 
 Registers and downloads the desired dataset.
 """
-function download(dataset_code)
-    dataset_url = dataurl(dataset_code)
-
+function download(dataid)
+    # TODO: Implement a `isvalid()` function to validate dataid
     # This uses register function of DataDeps package
     # Registers the dataset to be used
     register(
         DataDep(
-            dataset_code,
-            "GADM dataset for $dataset_code",
-            dataset_url,
+            dataid,
+            "GADM dataset for $dataid",
+            dataurl(dataid),
             post_fetch_method=DataDeps.unpack
         )
     )
 
     # downloading the dataset if not available
-    @info "Successfully downloaded the files: ", readdir(@datadep_str dataset_code; join=true)
+    @info "Successfully downloaded the files: ", readdir(@datadep_str dataid; join=true)
     return
 end
