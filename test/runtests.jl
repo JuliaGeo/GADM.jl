bcc233d8ebf04df6c6df8ef8c73d8a1048d9adbe
Aayush Joglekar <aayushjog@gmail.com>
2020-10-27 20:32:54 +0530

    [test] Set up test suite

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
 using Geography
 using Test
 
+testfiles = [
+    "download.jl"
+]
+
 @testset "Geography.jl" begin
-    # Write your tests here.
+for testfile in testfiles
+    include(testfile)
+  end
 end
