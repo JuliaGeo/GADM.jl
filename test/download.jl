71ac2bde7b33d1d5722610c6231980ebf1f12e4c
Aayush Joglekar <aayushjog@gmail.com>
2020-10-27 21:37:29 +0530

    [test] Fix tests for dataurl()

--- test/download.jl
+++ test/download.jl
@@ -1,17 +1,14 @@
-@testset "Download" begin
+@testset "dataurl" begin
     # dataurl returns a string on proper formatted code GADM_<Code>
     url = Geography.dataurl("GADM_IND")
     @test isequal(url, "https://biogeo.ucdavis.edu/data/gadm3.6/gpkg/gadm36_IND_gpkg.zip")
 
-    # dataurl returns nothing on improper formatted code GADM<Code>
-    url = Geography.dataurl("GADMIND")
-    @test isnothing(url)
+    # dataurl returns nothing on improper formatted code GADM<Code>    
+    @test_throws ArgumentError Geography.dataurl("GADMIND")
 
     # dataurl returns nothing when code doesn't contain GADM
-    url = Geography.dataurl("IND")
-    @test isnothing(url)
+    @test_throws ArgumentError Geography.dataurl("IND")
 
     # dataurl returns nothing on empty string
-    url = Geography.dataurl("")
-    @test isnothing(url)
+    @test_throws ArgumentError Geography.dataurl("")
 end
