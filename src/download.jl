a905f2b22809ba0b38ca8ba0c0b104f10eea435d
Aayush Joglekar <aayushjog@gmail.com>
2020-10-27 20:32:31 +0530

    [download.jl] Handle edge cases

--- src/download.jl
+++ src/download.jl
@@ -7,9 +7,12 @@ Returns the URL of the gpkg dataset of the country
 function dataurl(dataid)
     # Only accepts GADM Dataset Provider
     if contains(dataid, "GADM")
-        provider, country_code = split(dataid, '_')
-
-        "https://biogeo.ucdavis.edu/data/gadm3.6/gpkg/gadm36_$(country_code)_gpkg.zip"
+        try
+            provider, country_code = split(dataid, '_')
+            "https://biogeo.ucdavis.edu/data/gadm3.6/gpkg/gadm36_$(country_code)_gpkg.zip"
+        catch err
+            @error "Invalid dataset format, please use GADM_<country_code>."
+        end
     else
         @error "Dataset provider not supported. Please try \"GADM_<Country Code>\"."
     end
@@ -22,6 +25,10 @@ Registers and downloads the desired dataset.
 """
 function download(dataset_code)
     dataset_url = dataurl(dataset_code)
+
+    if dataset_url === nothing
+        return
+    end
 
     # This uses register function of DataDeps package
     # Registers the dataset to be used
