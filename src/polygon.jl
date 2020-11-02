de282b34dcf1f00830e7a70ebcc08752903641e8
Aayush Joglekar <aayushjog@gmail.com>
2020-11-03 00:11:52 +0530

    [polygon] resolve conflicts

--- src/polygon.jl
+++ src/polygon.jl
@@ -6,7 +6,7 @@ function getlevel(dataset::ArchGDAL.AbstractDataset, reqlevel::Int)
     # number of layers in the dataset
     nlayers = ArchGDAL.nlayer(dataset)
 
-    for i = 0:nlayers-1
+    for i = 0:nlayers - 1
         layer = ArchGDAL.getlayer(dataset, i)
         layername = ArchGDAL.getname(layer)
         layerlevel = split(layername, "_")[end]
@@ -15,7 +15,7 @@ function getlevel(dataset::ArchGDAL.AbstractDataset, reqlevel::Int)
         end
     end
 
-    throw(ArgumentError("valid levels for the given dataset are 0...$(nlayers-1)"))
+    throw(ArgumentError("valid levels for the given dataset are 0...$(nlayers - 1)"))
 end
 
 """
@@ -68,19 +68,62 @@ end
 
 
 """
-    get(code)
-Returns the MULTIPOLYGON data for the requested country.\n
-Input: ISO 3166 Alpha 3 Country Code
-"""
-function get(code)
-    # only uppercase country codes are accepted
-    isvalidcode(code) || throw(ArgumentError("please provide standard ISO 3166 Alpha 3 country codes"))
+    get(country, levels...)
+Returns the MULTIPOLYGON data for the requested region.\n
+Input: ISO3 Country Code, and further subdivisions\n
 
-    dataid = "GADM_$code"
+Examples:\n
+1. `get("IND")` - Returns polygon of India
+2. `get("IND", "Uttar Pradesh")` - Returns polygon of the state Uttar Pradesh
+3. `get("IND", "Uttar Pradesh", "Lucknow")` - Returns polygon of district Lucknow\n
+"""
+function get(country, levels...) 
+    # only uppercase country codes are accepted
+    isvalidcode(country) || throw(ArgumentError("please provide standard ISO 3 country codes"))
+
+    dataid = "GADM_$country"
 
     resource_data_path = download(dataid)
 
     dataset = extractdataset(resource_data_path)
 
-    extractgeometry(dataset)
+    # the number of varargs in levels is the required level for GADM dataset
+    required_level = length(levels)
+
+    # get layer of the required level
+    level = getlevel(dataset, required_level)
+
+    # get country level
+    if required_level == 0
+        return ArchGDAL.getfeature(level, ifeature) do feature
+            ArchGDAL.getgeom(feature)
+        end
+    end
+
+    nfeatures = ArchGDAL.nfeature(level)
+
+    # indexes of fields with entity full names
+    # name of i level entity is at index at indexes[i+1]
+    # Eg. Name of state (level 1) "Uttar Pradesh" will be found at index name_indexes[2], i.e. 3rd field
+    name_indexes = [0, 3, 6]
+
+    # looping on all features in the layer
+    for ifeature = 1:nfeatures
+
+        geometry = ArchGDAL.getfeature(level, ifeature) do feature
+
+            field_name = ArchGDAL.getfield(feature, name_indexes[required_level + 1])
+
+            # if query exists in field name, it matches and returns geometry
+            if occursin(lowercase(levels[end]), lowercase(field_name))
+                return ArchGDAL.getgeom(feature)
+            end
+        end
+
+        # if geometry is not nothing, requried shape has been found
+        if !isnothing(geometry)
+            return geometry
+        end
+    end
+    throw(ArgumentError("feature not found"))
 end
