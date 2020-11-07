96c6b0f2c53f9b1a52781090fbfc8c75a1b6ebb7
Aayush Joglekar <aayushjog@gmail.com>
2020-11-07 22:18:52 +0530

    Update GADM.jl

--- src/GADM.jl
+++ src/GADM.jl
@@ -9,7 +9,7 @@ using GeoInterface
     isvalidcode(str)
 
 Tells whether or not `str` is a valid
-ISO 3166 Alpha 3 country code. Valid 
+ISO 3166 Alpha 3 country code. Valid
 code examples are "IND", "USA", "BRA".
 """
 isvalidcode(str) = match(r"\b[A-Z]{3}\b", str) !== nothing
